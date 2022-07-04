68ecfa3e8aecd087ea7124c490d4fe4a281a171f
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-07-04 14:22:05 +0100

    Added more notes

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,5 @@
 * Default values used for server configuration
 */
 
-const SWEPH_PATH_DEFAULT: &str = "/usr/share/libswe/ephe";
+pub const SWEPH_PATH_DEFAULT: &str = "/usr/share/libswe/ephe";
 pub const DEFAULT_PORT: u32 = 8087;
