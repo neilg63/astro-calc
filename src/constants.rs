d7a0f39f40e0bdf7f80e2a3c9399554e29adbffd
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-07-12 20:24:45 +0100

    added test endpoint for geographic day starts

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,6 @@
 * Default values used for server configuration
 */
 
-pub const SWEPH_PATH_DEFAULT: &str = "/usr/share/libswe/ephe";
+pub const SWEPH_PATH_DEFAULT: &str = "/Users/neil/apps/findingyou/findingyou-api/src/astrologic/ephe";
+//pub const SWEPH_PATH_DEFAULT: &str = "/usr/share/libswe/ephe";
 pub const DEFAULT_PORT: u32 = 8087;
