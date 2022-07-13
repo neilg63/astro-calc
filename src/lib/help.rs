5174c0e386c7696c997054ae8cb5fdaf6088802b
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-07-13 08:42:40 +0100

    added iso datetime options to /positions endpoint

--- src/lib/help.rs
+++ src/lib/help.rs
@@ -26,6 +26,26 @@ pub fn endpoint_help() -> HashMap<String, HashMap<String,String>> {
       ("bodies", "comma-separated list of required bodies, all or core"),
       ("topo", "0 = geocentric, 1 topocentric"),
       ("eq", "0 = ecliptic, 1 equatorial"),
+      ("iso", "0 julian days (transition times), 1 ISO UTC datetime strings"),
+    ]
+  ));
+  help.insert("/chart-data".to_string(), info_map(
+    vec![
+      ("dt", "Date"),
+      ("loc", "lat,lng(,alt) coordinates, e.g. &loc=45.336,13.278,50 or just &loc=45.336,13.278"),
+      ("bodies", "comma-separated list of required bodies, all or core"),
+      ("topo", "0 = geocentric, 1 topocentric"),
+      ("eq", "0 = ecliptic only, 1 equatorial only, 2 show equatorial and ecliptic, 3 show azimuth and altitide and other planetary phenomena"),
+      ("ct", "include transits for the referenced bodies"),
+      ("hsys", "Comma-separated list of house system letters or `all` for all systems, default W (whole house system)"),
+      ("aya", "comma-separated list of available ayanamshas (see below). These are added as separate data-set and should be applied in a post processing stage via simple subtraction from the lng, ascendant or rectAscension values, which are always tropical (they may automatically applied in /positions)"),
+      ("iso", "0 julian days (transition times), 1 ISO UTC datetime strings"),
+      ("p2", "include progress longitudes based on 1 day = 1 year from referenced time. The progress day is mapped to years"),
+      ("p2yrs", "Number of years to capture for P2 data"),
+      ("p2ago", "Number of years ago for P2 start year"),
+      ("p2start", "Explcit start year for p2 data (alternative to above"),
+      ("p2py", "Number of p2 sample per year, default 2."),
+      ("p2bodies", "Bodies to captured for P2. These never include Uranus, Neptune, Pluto or asteroid. Narrow range to limit the payload"),
     ]
   ));
   help.insert("/progress".to_string(), info_map(
@@ -39,25 +59,6 @@ pub fn endpoint_help() -> HashMap<String, HashMap<String,String>> {
       ("dspan", "number of days per sample, overrides pd above for longer spans, max 1000 samples"),
       ("topo", "0 = geocentric, 1 topocentric"),
       ("eq", "0 = ecliptic only, 1 equatorial only"),
-    ]
-  ));
-  help.insert("/chart-data".to_string(), info_map(
-    vec![
-      ("dt", "Date"),
-      ("loc", "lat,lng(,alt) coordinates, e.g. &loc=45.336,13.278,50 or just &loc=45.336,13.278"),
-      ("bodies", "comma-separated list of required bodies, all or core"),
-      ("topo", "0 = geocentric, 1 topocentric"),
-      ("eq", "0 = ecliptic only, 1 equatorial only, 2 show equatorial and ecliptic, 3 show azimuth and altitide and other planetary phenomena"),
-      ("ct", "include transits for the referenced bodies"),
-      ("hsys", "Comma-separated list of house system letters or `all` for all systems, default W (whole house system)"),
-      ("aya", "comma-separated list of available ayanamshas (see below). These are added as separate data-set and should be applied in a post processing stage via simple subtraction from the lng, ascendant or rectAscension values, which are always tropical (they may automatically applied in /positions)"),
-      ("iso", "0 show all times as julian days, 1 show transitions times as ISO UTC datetime strings"),
-      ("p2", "include progress longitudes based on 1 day = 1 year from referenced time. The progress day is mapped to years"),
-      ("p2yrs", "Number of years to capture for P2 data"),
-      ("p2ago", "Number of years ago for P2 start year"),
-      ("p2start", "Explcit start year for p2 data (alternative to above"),
-      ("p2py", "Number of p2 sample per year, default 2."),
-      ("p2bodies", "Bodies to captured for P2. These never include Uranus, Neptune, Pluto or asteroid. Narrow range to limit the payload"),
     ]
   ));
   help.insert("/transitions".to_string(), info_map(
