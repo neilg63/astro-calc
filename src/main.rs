e766c3ae53cfedbfc732c30b94bcf288ab6ac57d
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-07-22 15:42:26 +0100

    moved more endpoints to dedicate route files and added indianTime option to chart-data

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,13 @@ use libswe_sys::swerust::{
 use serde::{Serialize, Deserialize};
 use serde_json::*;
 use clap::Parser;
-use lib::{core::*, transitions::*, models::{geo_pos::*, date_info::*, general::*}, utils::{validators::*, converters::*}, planet_stations::{match_all_planet_stations_range, BodySpeedSet}};
+use lib::{models::{date_info::*}};
 use extensions::swe::{set_sid_mode};
-use actix_web::{get, App, HttpServer, Responder, web::{self, Query, Json}};
+use actix_web::{App, HttpServer, Responder, web::{self, Json}};
 use std::path::Path;
-use lib::julian_date::{current_datetime_string, current_year};
 use constants::*;
-use query_params::*;
 use help::*;
-use routes::{chart_data::*, transitions::*};
+use routes::{chart_data::*, transitions::*, planet_stations::*, datetime::*, progress_synastry::*};
 
 /// Astrologic engine config
 #[derive(Parser, Debug)]
@@ -46,74 +44,6 @@ struct Args {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct AppData {
   path: String,
-}
-
-#[get("/jd/{dateref}")]
-async fn date_info(dateref: web::Path<String>) -> impl Responder {
-  let date_str = dateref.as_str();
-  let info = if is_decimal_str(date_str) { DateInfo::new_from_jd(date_str.parse::<f64>().unwrap()) } else { DateInfo::new(date_str) };
-  web::Json(json!(info))
-}
-
-#[get("/date")]
-async fn date_info_geo(params: Query<InputOptions>) -> impl Responder {
-  let date = to_date_object(&params);
-  let loc: String = params.loc.clone().unwrap_or("0,0".to_string());
-  let geo = if let Some(geo_pos) = loc_string_to_geo(loc.as_str()) { geo_pos } else { GeoPos::zero() };
-  let tz_secs =  params.tzs.clone().unwrap_or(0i16);
-  let iso_mode: bool = params.iso.clone().unwrap_or(0) > 0;
-  let offset_secs = if tz_secs != 0i16 { Some(tz_secs) } else { None };
-  let (indian, prev, base, next, calc_offset_secs) = to_indian_time_with_transitions(date.jd, geo, offset_secs, iso_mode);
-  Json(json!({ "date": date, "indianTime": indian,  "offsetSecs": calc_offset_secs, "sun": { "prev": prev, "current": base, "next": next } }))
-}
-
-
-#[get("/test-geo-start")]
-async fn test_geo_start(params: Query<InputOptions>) -> impl Responder {
-  let loc: String = params.loc.clone().unwrap_or("0,0".to_string());
-  let geo = if let Some(geo_pos) = loc_string_to_geo(loc.as_str()) { geo_pos } else { GeoPos::zero() };
-  let date = to_date_object(&params);
-  let start_jd = start_jd_geo(date.jd, geo.lng);
-  let start = DateInfo::new_from_jd(start_jd);
-  Json(json!({ "date": date, "dayStart": start, "lng": geo.lng, "lat": geo.lat }))
-}
-
-#[get("/planet-stations")]
-async fn planet_stations_progress(params: Query<InputOptions>) -> impl Responder {
-  reset_ephemeris_path();
-  let micro_interval = time::Duration::from_millis(30);  
-  let date = to_date_object(&params);
-  let def_keys = vec!["me", "ve", "ma", "ju", "sa", "ur", "ne", "pl"];
-  let key_string: String = params.bodies.clone().unwrap_or("".to_string());
-  let keys = body_keys_str_to_keys_or(key_string, def_keys);
-  let dateref_end: String = params.dt2.clone().unwrap_or(current_datetime_string());
-  let iso_mode: bool = params.iso.clone().unwrap_or(0) > 0;
-  let end_date = DateInfo::new(dateref_end.to_string().as_str());
-  let items: Vec<BodySpeedSet> = match_all_planet_stations_range(date.jd, end_date.jd, to_str_refs(&keys), iso_mode);
-  let valid = items.len() > 0;
-  thread::sleep(micro_interval);
-  Json(json!({ "valid": valid, "start": date,  "end": end_date, "items": items }))
-}
-
-#[get("/p2")]
-async fn progress_synastry(params: Query<InputOptions>) -> impl Responder {
-  reset_ephemeris_path();
-  let micro_interval = time::Duration::from_millis(30);  
-  let loc: String = params.loc.clone().unwrap_or("0,0".to_string());
-  let geo = if let Some(geo_pos) = loc_string_to_geo(loc.as_str()) { geo_pos } else { GeoPos::zero() };
-  let date = to_date_object(&params);
-  let p2_ago: u8 = params.p2ago.clone().unwrap_or(1);
-  let p2_start: u16 = params.p2start.clone().unwrap_or(0);
-  let p2_start_year = if p2_start > 1800 { p2_start as u32 } else { current_year() as u32 - p2_ago as u32 };
-  let p2_years: u8 = params.p2yrs.clone().unwrap_or(3);
-  let p2_per_year: u8 = params.p2py.clone().unwrap_or(2);
-  let def_keys = vec!["su", "mo", "ma", "me", "ju", "ve", "sa"];
-  let key_string: String = params.bodies.clone().unwrap_or("".to_string());
-  let keys = body_keys_str_to_keys_or(key_string, def_keys);
-  let items: Vec<ProgressItemSet> = get_bodies_p2(date.jd, keys, p2_start_year, p2_years as u16, p2_per_year);
-  let valid = items.len() > 0;
-  thread::sleep(micro_interval);
-  Json(json!({ "valid": valid, "date": date,  "start_year": p2_start_year, "years": p2_years, "per_year": p2_per_year, "geo": geo, "items": items }))
 }
 
 async fn date_now() -> impl Responder {
@@ -167,7 +97,7 @@ async fn main()  -> std::io::Result<()> {
           .service(bodies_progress)
           .service(body_positions)
           .service(chart_data_flexi)
-          .service(progress_synastry)
+          .service(progress_synastry_items)
           .service(list_sun_transitions)
           .service(pheno_data)
           .service(list_transitions)
